d215fbb6cd688823b9e90cb76da4b315ef424c8d
Casualet <shaoyiwenet@gmail.com>
2017-10-19 14:02:19 +0800

    add

--- get_graph_link.sh
+++ get_graph_link.sh
@@ -2,6 +2,6 @@ ls -rt $(find ./blogGraph/ -type f) > re
 
 s="![](https://yiwenshao.github.io/"
 
-cat re|while read line;do echo $s${line:2}"]"; done;
+cat re|while read line;do echo $s${line:2}")"; done;
 
 rm re
